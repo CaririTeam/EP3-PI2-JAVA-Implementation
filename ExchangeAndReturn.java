929d8cd1addf1c64a91878685d496fd46cffb2a0
Francisco Eudo <franciscoeudo25@yahoo.com>
2024-09-10 19:48:48 -0300

    Implementação da classe ExchangeAndReturn

---
+++ ExchangeAndReturn.java
@@ -0,0 +1,81 @@
+import java.util.Date;
+import java.util.List;
+
+public class ExchangeAndReturn {
+    private int idExchangeAndReturn;
+    private int idSale;
+    private List<ProductToOperate> itens;
+    private Date date;
+    private String observations;
+    private double voucher;
+
+    public ExchangeAndReturn(int idExchangeAndReturn, int idSale, List<ProductToOperate> itens, 
+                             Date date, String observations, double voucher) {
+        this.idExchangeAndReturn = idExchangeAndReturn;
+        this.idSale = idSale;
+        this.itens = itens;
+        this.date = date;
+        this.observations = observations;
+        this.voucher = voucher;
+    }
+
+    public int getIdExchangeAndReturn() {
+        return idExchangeAndReturn;
+    }
+
+    public void setIdExchangeAndReturn(int idExchangeAndReturn) {
+        this.idExchangeAndReturn = idExchangeAndReturn;
+    }
+
+    public int getIdSale() {
+        return idSale;
+    }
+
+    public void setIdSale(int idSale) {
+        this.idSale = idSale;
+    }
+
+    public List<ProductToOperate> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ProductToOperate> itens) {
+        this.itens = itens;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public double getVoucher() {
+        return voucher;
+    }
+
+    public void setVoucher(double voucher) {
+        this.voucher = voucher;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeAndReturn{" +
+               "idExchangeAndReturn=" + idExchangeAndReturn +
+               ", idSale=" + idSale +
+               ", itens=" + itens +
+               ", date=" + date +
+               ", observations='" + observations + '\'' +
+               ", voucher=" + voucher +
+               '}';
+    }
+}
--- ProductReceived.java
+++ ProductReceived.java
@@ -7,5 +7,53 @@ public class ProductReceived {
     private List<ProductToOperate> itens;
     private String observations;
 
+    public ProductReceived(int idReceiving, Date date, List<ProductToOperate> itens, String observations) {
+        this.idReceiving = idReceiving;
+        this.date = date;
+        this.itens = itens;
+        this.observations = observations;
+    }
+
+    public int getIdReceiving() {
+        return idReceiving;
+    }
+
+    public void setIdReceiving(int idReceiving) {
+        this.idReceiving = idReceiving;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<ProductToOperate> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ProductToOperate> itens) {
+        this.itens = itens;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductReceived{" +
+               "idReceiving=" + idReceiving +
+               ", date=" + date +
+               ", itens=" + itens +
+               ", observations='" + observations + '\'' +
+               '}';
+    }
 
 }
