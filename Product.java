581497a160b77528e628d433bdac8b18c6cb2c78
Wagner Lavandoski <wagner.flpadilha@gmail.com>
2024-09-10 17:49:26 -0300

    Criação Classe Produto

---
+++ Product.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+import java.util.List;
+
+public class Product {
+    private int idProduct;
+    private String name;
+    private String productCategory;
+    private double cost;
+    private double price;
+    private String manufacturer;
+    private Date manufacturingDate;
+    private Date expirationDate;
+    private double minimunStock;
+    private double maximunStock;
+    private int corridor;
+    private String shelf;
+    private String unitMeasurement;
+    private List<Supplier> suppliers;
+
+    
+}
+
+
