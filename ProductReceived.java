0beb9f88f9880cea3c05d3382b14b8b1a558fad4
Wagner Lavandoski <wagner.flpadilha@gmail.com>
2024-09-10 17:53:30 -0300

    Criação Classe Produto Recebido

---
+++ ProductReceived.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+import java.util.List;
+
+public class ProductReceived {
+    private int idReceiving;
+    private Date date;
+    private List<ProductToOperate> itens;
+    private String observations;
+
+
+}
