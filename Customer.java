3ee0c1b13d76dc9a3c3e7192461a858e0d7dde6d
Wagner Lavandoski <wagner.flpadilha@gmail.com>
2024-09-10 16:37:04 -0300

    Atualização Customer

--- Customer.java
+++ Customer.java
@@ -4,5 +4,43 @@ public class Customer {
     private String phone;
     private Address address;
 
+    public Customer(String name, String cpf, String phone, Address address) {
+        this.name = name;
+        this.cpf = cpf;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
 }
